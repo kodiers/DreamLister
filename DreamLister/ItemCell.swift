104b12bf2be0d1d2f4d6ff14ece8d22cbd81b4e2
VY <kodiers@gmail.com>
2017-07-13 04:00:54 +0300

    Added layout and views

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,29 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Viktor Yamchinov on 13/07/2017.
+//  Copyright © 2017 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DreamLister/MainVC.swift
@@ -0,0 +1,42 @@
+//
+//  MainVC.swift
+//  DreamLister
+//
+//  Created by Viktor Yamchinov on 12/07/2017.
+//  Copyright © 2017 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+
+}
+
